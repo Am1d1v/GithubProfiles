b5bc657b893fcce984eb49f754bfee0aa177d933
Am1d1v <dimadiman1adima@gmail.com>
2023-12-16 20:28:49 +0300

    .

--- script.js
+++ script.js
@@ -3,15 +3,19 @@
 const APIURL = 'https://api.github.com/users/';
 const form = document.querySelector('#form');
 const search = document.querySelector('#search');
+const main = document.querySelector('#main');
 
 
 // Fetch a user data
 async function getUser(username){
     try {
         const {data} = await axios(APIURL + username);
-        console.log(data);
+        createUserCard(data)
     } catch (error) {
-        console.log(error);
+        if(error.response.status = 404){
+          createErrorCard('No profile with this username');
+        }
+        
     }
 }
 //getUser('Am1d1v');
@@ -24,5 +28,46 @@ form.addEventListener('submit', (event) => {
     
     if(user){
         getUser(user);
+        search.value = '';
     }
 })
+
+// Create dynamic card of user
+function createUserCard(user){
+    const cardHTML = `
+    <div class="card">
+
+        <div>
+            <img src="${user.avatar_url}" alt="profileImage" class="avatar">
+        </div>
+
+        <div class="user-info">
+            <h2>${user.name ? user.name : user.login}</h2>
+            <p>${user.bio ? user.bio : 'User has no biography'}</p>
+
+            <ul>
+                <li>${user.followers} <strong>Followers</strong></li>
+                <li>${user.following} <strong>Followings</strong></li>
+                <li>${user.public_repos} <strong>Repos</strong></li>
+            </ul>
+
+            <div id="repos">
+
+            </div>
+        </div>
+
+    </div>
+`;
+
+    main.innerHTML = cardHTML;
+}
+
+// Error Card. Pop up if user profile is not found
+function createErrorCard(message){
+    const cardHTML = `
+        <div class="card">
+            <h1>${message}</h1>
+        </div>
+    `
+    main.innerHTML = cardHTML;
+}
